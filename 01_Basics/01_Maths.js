57cf50966ede788401ca5e340130161bf3c1a68f
Tusar kumar Parida <tusarkumarparida9@gmail.com>
2024-06-13 17:42:59 +0530

    Added min and max random number

--- 01_Basics/01_Maths.js
+++ 01_Basics/01_Maths.js
@@ -9,6 +9,12 @@ console.log(Math.min(4,5,2))
 console.log(Math.max(41,5,2))
 
 // Finding a random number between min_value and max_value 
+
 const min = 10;
 const max = 20;
 console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// without add min1_value we cant find a random number between min_value and max_value
+const min1= 10;
+const max1= 20;
+console.log(Math.floor(Math.random() * (max1 - min1 + 1)));
