04547c606f96cdbe975c7c48c8adb28be9c44969
Tusar kumar Parida <tusarkumarparida9@gmail.com>
2024-06-13 17:19:32 +0530

    Added commit

---
+++ 01_Basics/01_Maths.js
@@ -0,0 +1,13 @@
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(4.7))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.5))
+// console.log("Mix & Max")
+// console.log(Math.min(4,5,2))
+// console.log(Math.max(41,5,2))
+
+// Finding a random number between min_value and max_value 
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
