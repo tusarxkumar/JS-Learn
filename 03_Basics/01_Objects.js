34d617013e24fddd4f8019ff56cc86858b10c1d9
Tusar kumar Parida <tusarkumarparida9@gmail.com>
2024-06-19 15:20:29 +0530

    Resolved merge conflict in JS_01_Objects.js

--- 03_Basics/01_Objects.js
+++ 03_Basics/01_Objects.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //How to access the symbol from object
 const mySym = Symbol("Key1")
 // [mySym]:"Key1"
@@ -18,25 +17,4 @@ const JsUser={
 console.log(JsUser["full name"])
 console.log(JsUser["Email"])
 console.log(JsUser[mySym])
-=======
-//How to access the symbol from object
-const mySym = Symbol("Key1")
-// [mySym]:"Key1"
-
-const JsUser={
-    [mySym]:"CSK",
-    name: "Tushar",
-    "full name": "Tushar kumar Parida",
-    age: 18,
-    Email: "tushar@gmail.com",
-    location:"Odisha",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday","Tuesday"]
-}
-
-//prefer way of accessing objects
-console.log(JsUser["full name"])
-console.log(JsUser["Email"])
-console.log(JsUser[mySym])
->>>>>>> 4600c0856855101c5abcee06aa41677553111dbc
 console.log(JsUser)
