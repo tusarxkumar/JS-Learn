3568369976c67f17fd60cb846923370b5da584f7
Tusar kumar Parida <tusarkumarparida9@gmail.com>
2024-06-25 12:44:27 +0530

    Template Literals

---
+++ 10_Template-Literal/main.js
@@ -0,0 +1,16 @@
+//Template Literals
+let num1=10
+let num2=30
+let result = num1 + num2
+
+// console.log("the addition of " + num1 + " + " + num2 + " is " + result);
+
+//Instead of using concatenation
+console.log(` The addition of ${num1} + ${num2} is ${result}`);
+
+// Also instead of using \n for new line you can use ` enter to new line`
+
+console.log("My name is \nTusar");
+
+console.log(`My name is 
+Tusar Kumar Parida`)
